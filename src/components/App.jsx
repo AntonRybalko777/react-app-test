c69d17e510470ab6579edf395c73c960535ddde8
Anton Rybalko <126842693+AntonRybalko777@users.noreply.github.com>
2023-11-30 18:20:53 +0200

    added global styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import { Layout } from './Layout/Layout';
 import Main from 'pages/main';
 import Admin from 'pages/admin';
 import { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { fetchData } from 'redux/operations';
 
 export const App = () => {
