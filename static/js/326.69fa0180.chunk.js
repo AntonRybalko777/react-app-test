b16da0b6874e879d4dca65bf49713c6ae2a733fa
AntonRybalko777 <126842693+AntonRybalko777@users.noreply.github.com>
2023-12-01 16:53:43 +0000

    Deploying to gh-pages from @ AntonRybalko777/react-app-test@009fbbe7b537407e18808cd748ff8daf3846ec6f 🚀

---
+++ static/js/326.69fa0180.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[326],{326:function(e,t,n){n.r(t),n.d(t,{default:function(){return u}});var r=n(184);function u(){return(0,r.jsx)("div",{children:"This is Admin page"})}}}]);
+//# sourceMappingURL=326.69fa0180.chunk.js.map
